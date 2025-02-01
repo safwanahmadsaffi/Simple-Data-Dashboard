b55f3f4c57421b09666973f64dd25b0de1c37f11
Safwan Ahmad <sahmedsaffi@gmail.com>
2025-02-01 19:10:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("Prddicitive Buisness  Data Dashboard")
+st.title("Simple Data Dashboard")
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
